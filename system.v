e140ecf0d71cc3841b4fc15b327df17f55bf85c8
Oliver-K3003 <60487795+Oliver-K3003@users.noreply.github.com>
2023-01-22 18:25:55 -0500

    Added Project Files

---
+++ system.v
@@ -0,0 +1,12 @@
+`timescale 1ns/10ps
+//Test register
+module Register(d, q, clr, clk, enable);
+	input [31:0] d, clr, clk, enable;
+	output [31:0] q;
+	//Assuming active high & async clear
+	always @ (posedge clk or posedge clr or posedge enable)
+		if(clr and enable)
+			q <= 0;
+		else if(clk and enable)
+			q <= d;
+	end;
