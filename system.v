b5903fb24a58f36cdd70f0ece595e89d9cc6b4cf
Oliver-K3003 <60487795+Oliver-K3003@users.noreply.github.com>
2023-01-23 14:46:14 -0500

    Added register file

--- system.v
+++ system.v
@@ -1,12 +1,4 @@
 `timescale 1ns/10ps
 //Test register
-module R1(d, q, clr, clk, enable);
-	input [31:0] d, clr, clk, enable;
-	output [31:0] q;
-	//Assuming active high & async clear
-	always @ (posedge clk or posedge clr or posedge enable)
-		if(clr and enable)
-			q <= 0;
-		else if(clk and enable)
-			q <= d;
-	end;
+
+
