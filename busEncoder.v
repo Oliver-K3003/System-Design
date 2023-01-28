d7be282476f54ab8ce0bc4c3b812f27a3fe1f429
Oliver-K3003 <60487795+Oliver-K3003@users.noreply.github.com>
2023-01-28 18:53:55 -0500

    Got Encoder Working

--- busEncoder.v
+++ busEncoder.v
@@ -1,62 +1,41 @@
-module busEncoder(en, in, out);
-	input en;
-	input [31:0] in;
-	output reg [4:0]out;
-	always @(en, in)
+`timescale 1ns/10ps
+
+module busEncoder(input wire [31:0] in, input en, output reg [4:0] out);
+	always @(in or en)
 	begin
 		if(en==1)
 			begin
-				if(in[31]==1)      out=5'b11111;
-				else if(in[30]==1) out=5'b11110;
-				else if(in[29]==1) out=5'b11101;
-				else if(in[28]==1) out=5'b11100;
-				else if(in[27]==1) out=5'b11011;
-				else if(in[26]==1) out=5'b11010;
-				else if(in[25]==1) out=5'b11001;
-				else if(in[24]==1) out=5'b11000;
-				//This is for all the connections (above connections can be used for other things if needed)
-				else if(in[23]==1) out=5'b10111;
-				else if(in[22]==1) out=5'b10110;
-				else if(in[21]==1) out=5'b10101;
-				else if(in[20]==1) out=5'b10100;
-				else if(in[19]==1) out=5'b10011;
-				else if(in[18]==1) out=5'b10010;
-				else if(in[17]==1) out=5'b10001;
-				else if(in[16]==1) out=5'b10000;
-				else if(in[15]==1) out=5'b01111;
-				else if(in[14]==1) out=5'b01110;
-				else if(in[13]==1) out=5'b01101;
-				else if(in[12]==1) out=5'b01100;
-				else if(in[11]==1) out=5'b01011;
-				else if(in[10]==1) out=5'b01010;
-				else if(in[9]==1)  out=5'b01001;
-				else if(in[8]==1)  out=5'b01000;
-				else if(in[7]==1)  out=5'b00111;
-				else if(in[6]==1)  out=5'b00110;
-				else if(in[5]==1)  out=5'b00101;
-				else if(in[4]==1)  out=5'b00100;
-				else if(in[3]==1)  out=5'b00011;
-				else if(in[2]==1)  out=5'b00010;
-				else if(in[1]==1)  out=5'b00001;
-				//default value if nothing is selected (no bus driver)
-				else   			  out=5'b00000;
+				case(in)
+					//all connections past 23 can be used for other things if needed
+					32'h00000001 : out <= 5'd0;   
+         		32'h00000002 : out <= 5'd1;     
+	 				32'h00000004 : out <= 5'd2;      
+					32'h00000008 : out <= 5'd3;     
+					32'h00000010 : out <= 5'd4;     
+					32'h00000020 : out <= 5'd5;      
+					32'h00000040 : out <= 5'd6;    
+					32'h00000080 : out <= 5'd7;     
+					32'h00000100 : out <= 5'd8;     
+					32'h00000200 : out <= 5'd9;      
+					32'h00000400 : out <= 5'd10;    
+					32'h00000800 : out <= 5'd11;   
+					32'h00001000 : out <= 5'd12;    
+					32'h00002000 : out <= 5'd13;  
+					32'h00004000 : out <= 5'd14; 
+					32'h00008000 : out <= 5'd15; 
+	 				32'h00010000 : out <= 5'd16;     
+					32'h00020000 : out <= 5'd17;     
+					32'h00040000 : out <= 5'd18;  
+					32'h00080000 : out <= 5'd19;   
+					32'h00100000 : out <= 5'd20;     
+					32'h00200000 : out <= 5'd21;    
+					32'h00400000 : out <= 5'd22; 
+					32'h00800000 : out <= 5'd23;
+					//default value if nothing is selected     
+	  				default: out <= 5'd31;
+				endcase
 			end
 			//high impedance fallthrough if the encoder enable is zero
 		else	out=5'bzzzzz;
 	end
 endmodule
-
-/*
-module encoder_32_5(input [31:0] input, output [4:0] output);
-    always_comb begin
-        case (input)
-            32'b00000: output = 5'b00000;
-            32'b00001: output = 5'b00001;
-            32'b00010: output = 5'b00010;
-            // ...
-            32'b11111: output = 5'b11111;
-            default: output = 5'b00000;
-        endcase
-    end
-endmodule
-*/
--- busMUX.v
+++ busMUX.v
@@ -1,4 +1,8 @@
-module busMUX(input [31:0] r0, 
+`timescale 1ns/10ps
+
+module busMUX(
+			  // general purpose registers
+			  input [31:0] r0, 
 			  input [31:0] r1, 
 			  input [31:0] r2, 
 			  input [31:0] r3, 
@@ -13,7 +17,8 @@ module busMUX(input [31:0] r0,
 			  input [31:0] r12, 
 			  input [31:0] r13, 
 			  input [31:0] r14, 
-			  input [31:0] r15, 
+			  input [31:0] r15,
+			  
 			  input [31:0] hi, 
 			  input [31:0] lo, 
 			  input [31:0] zhi, 
@@ -22,55 +27,40 @@ module busMUX(input [31:0] r0,
 			  input [31:0] mdr, 
 			  input [31:0] inport, 
 			  input [31:0] signExt,
-			  input [4:0] sel,
+			  
+			  // select signal
+			  input wire [4:0] sel,
+			  
+			  // mux output register
 			  output reg [31:0] muxOut);
 
-always @ (r0 or r1 or r2 or r3 or r5 or r6 or r7 or r8 or r9 or r10 or r11 or r12 or r13 or r14 or r15 or hi or lo or zhi or zlo or pc or mdr or inport or signExt or sel) 
-begin
-	case (sel)
-		5'b00001 : muxOut <= r1;
-		5'b00010 : muxOut <= r2;
-		5'b00011 : muxOut <= r3;
-		5'b00100 : muxOut <= r4;
-		5'b00101 : muxOut <= r5;
-		5'b00110 : muxOut <= r6;
-		5'b00111 : muxOut <= r7;
-		5'b01000 : muxOut <= r8;
-		5'b01001 : muxOut <= r9;
-		5'b01010 : muxOut <= r10;
-		5'b01011 : muxOut <= r11;
-		5'b01100 : muxOut <= r12;
-		5'b01101 : muxOut <= r13;
-		5'b01110 : muxOut <= r14;
-		5'b01111 : muxOut <= r15;
-		5'b10000 : muxOut <= hi;
-		5'b10001 : muxOut <= lo;
-		5'b10010 : muxOut <= zhi;
-		5'b10011 : muxOut <= zlo;
-		5'b10100 : muxOut <= pc;
-		5'b10101 : muxOut <= mdr;
-		5'b10110 : muxOut <= inport;
-		5'b10111 : muxOut <= signExt;
-	endcase
-end
+	always @(*) 
+	begin
+		case (sel)
+			5'b00001 : muxOut <= r1[31:0];
+			5'b00010 : muxOut <= r2[31:0];
+			5'b00011 : muxOut <= r3[31:0];
+			5'b00100 : muxOut <= r4[31:0];
+			5'b00101 : muxOut <= r5[31:0];
+			5'b00110 : muxOut <= r6[31:0];
+			5'b00111 : muxOut <= r7[31:0];
+			5'b01000 : muxOut <= r8[31:0];
+			5'b01001 : muxOut <= r9[31:0];
+			5'b01010 : muxOut <= r10[31:0];
+			5'b01011 : muxOut <= r11[31:0];
+			5'b01100 : muxOut <= r12[31:0];
+			5'b01101 : muxOut <= r13[31:0];
+			5'b01110 : muxOut <= r14[31:0];
+			5'b01111 : muxOut <= r15[31:0];
+			5'b10000 : muxOut <= hi[31:0];
+			5'b10001 : muxOut <= lo[31:0];
+			5'b10010 : muxOut <= zhi[31:0];
+			5'b10011 : muxOut <= zlo[31:0];
+			5'b10100 : muxOut <= pc[31:0];
+			5'b10101 : muxOut <= mdr[31:0];
+			5'b10110 : muxOut <= inport[31:0];
+			5'b10111 : muxOut <= signExt[31:0];
+			default: muxOut <= 32'd0;
+		endcase
+	end
 endmodule
-
-/*
-module mux32_1(
-input [5:0] sel, // selector input, selects which input to forward
-input [31:0] in, // 32 input signals
-output reg out // output signal
-);
-
-always @(*) begin
-  case (sel)
-    6'b000000: out = in[0];
-    6'b000001: out = in[1];
-    6'b000010: out = in[2];
-    ...
-    6'b111111: out = in[31];
-  endcase
-end
-
-endmodule
-*/
